2687042fdb12f977d1e3d8e734df40b95f67014e
stevenacz <scoailazaa@gmail.com>
2021-02-20 14:23:11 -0500

    Main added

--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import CardsContainer from '../CardsContainer'
+import { store } from '../../data/store'
 
 const Main = () => {
   return (
-    <>
-      <h1>Main</h1>
-    </>
+    <main className="main">
+      {
+        store.map( ({ id, date, cards }) => (
+          <CardsContainer
+            key={ id }
+            date={ date }
+            cards={ cards }
+          />
+        ))
+      }
+    </main>
   )
 }
 
