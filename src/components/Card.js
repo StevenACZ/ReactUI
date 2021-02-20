5c8dde2b21c8e305cc574268b664c57a1a55f5a8
stevenacz <scoailazaa@gmail.com>
2021-02-20 14:23:35 -0500

    Card improved

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({
+  description,
+  img,
+  title
+}) => {
   return (
-    <div>
-      <h1>Card</h1>
+    <div className="card">
+      <img className="card__img" src={ img }/>
+
+      <div className="card__info">
+        <p>{ title }</p>
+        <h2>{ description }</h2>
+      </div>
     </div>
   )
 }
